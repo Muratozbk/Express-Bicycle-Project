872b2c9bd20ec1294eec92646ce4efb6176872e1
muratozbk <slipstream11@yandex.com>
2023-09-06 14:22:59 +0300

    post request with body parser

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 const express = require('express');
 const app = express();
 
+//parse application
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 app.get('/', (req, res) => {
      res.send('<h1>Hi Murat</h1>')
 });
@@ -10,6 +14,13 @@ app.get('/about', (req, res) => {
      res.json({
           name: 'Murat'
      })
+});
+
+app.post('/login', (req, res) => {
+     console.log(req.body.email);
+     console.log(req.body.password);
+
+     res.send('User login Successfully')
 })
 
 app.listen(3000, () => console.log('Server is running at port 3000'));
